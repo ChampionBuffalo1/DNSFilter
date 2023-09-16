bde19a6bed9d19c43e988b95c0f23d47d5df3957
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-09-16 20:26:03 +0530

    Using default dns resolve for now

--- src/main/java/com/champ/dns/DNSHandler.java
+++ src/main/java/com/champ/dns/DNSHandler.java
@@ -1,16 +1,18 @@
 package com.champ.dns;
 
+import java.net.InetAddress;
 import java.util.logging.Logger;
+import java.net.UnknownHostException;
 
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.dns.DnsSection;
+import io.netty.handler.codec.dns.DnsQuestion;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.dns.DnsRecordType;
 import io.netty.handler.codec.dns.DatagramDnsQuery;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.dns.DatagramDnsResponse;
 import io.netty.handler.codec.dns.DefaultDnsRawRecord;
-import io.netty.handler.codec.dns.DnsQuestion;
 
 public class DNSHandler extends SimpleChannelInboundHandler<DatagramDnsQuery> {
     private static final Logger logger = Logger.getLogger(DNSHandler.class.getName());
@@ -18,16 +20,19 @@ public class DNSHandler extends SimpleChannelInboundHandler<DatagramDnsQuery> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, DatagramDnsQuery query) {
         DnsQuestion dnsQuestion = query.recordAt(DnsSection.QUESTION);
-
         DatagramDnsResponse response = new DatagramDnsResponse(query.recipient(), query.sender(), query.id());
+        try {
+            InetAddress addr = InetAddress.getByName(dnsQuestion.name());
+             response.addRecord(DnsSection.ANSWER, new DefaultDnsRawRecord(
+                    dnsQuestion.name(),
+                    DnsRecordType.A,
+                    60,
+                    Unpooled.buffer().writeBytes(addr.getAddress())));
 
-        response.addRecord(DnsSection.ANSWER, new DefaultDnsRawRecord(
-                dnsQuestion.name(),
-                DnsRecordType.A,
-                60,
-                Unpooled.buffer()
-                        .writeBytes(new byte[] { 127, 0, 0, 1 })
-        ));
+        } catch (UnknownHostException unk) {
+            // TODO: Return something like DNS error(?)
+            logger.severe(String.format("Unknown host for address: %s", dnsQuestion.name()));
+        }
 
         ctx.writeAndFlush(response).addListener((future) -> {
             if (!future.isSuccess())
